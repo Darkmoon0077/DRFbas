d9d9de928e70597453e4a22d439e64a9a8794f77
Darkmoon077 <darkmoon0077@gmail.com>
2024-02-13 17:18:56 +0600

    sadasd

--- authorz/forms.py
+++ authorz/forms.py
@@ -36,9 +36,9 @@ class UserLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
-            self.fields['username'].widget.attrs['placeholder'] = 'Логин пользователя'
+            self.fields['email'].widget.attrs['placeholder'] = 'Email пользователя'
             self.fields['password'].widget.attrs['placeholder'] = 'Пароль пользователя'
-            self.fields['username'].label = 'Логин'
+            self.fields['email'].label = 'Email'
             self.fields[field].widget.attrs.update({
                 'class': 'form-control',
                 'autocomplete': 'off'
