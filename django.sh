af76ac640c4a002633a04be3e03afec64208d781
Darkmoon077 <darkmoon0077@gmail.com>
2024-02-16 12:13:48 +0600

    dump

--- django.sh
+++ django.sh
@@ -6,7 +6,9 @@ echo ====================================
 echo "Starting Migrations..."
 python manage.py migrate
 echo ====================================
-
+echo "dumping data..."
+python manage.py loaddata dump.json
+echo ====================================
 
 echo "Starting Server..."
 python manage.py runserver 0.0.0.0:8000
